b76a783c75b631ba2c624503eb5c886da7836faf
Furkan Bozkurt <furkanbozkurt55@gmail.com>
2022-06-15 19:56:57 +0300

    Add unit tests

---
+++ src/Tests/Contacts/Contacts.UnitTests/Application/CreateContactHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using Services.Contacts.Application.Commands;
+using Services.Contacts.Application.Commands.Handler;
+using Services.Contacts.Domain.Aggregates.Contact;
+using Services.Contacts.Domain.Exceptions;
+using Services.Contacts.Domain.Factories;
+using Shared.Abstractions.Commands;
+using Shouldly;
+using Xunit;
+
+namespace Contacts.UnitTests.Application;
+
+public class CreateContactHandlerTests
+{
+    private Task Act(CreateContact command)
+        => _commandHandler.HandleAsync(command);
+
+    [Fact]
+    public async Task HandleAsync_Throws_EmptyContactFirstNameException_When_First_Name_Is_Empty()
+    {
+        var command = new CreateContact(Guid.NewGuid(), string.Empty, "Bozkurt");
+
+        var exception = await Record.ExceptionAsync(() => Act(command));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<EmptyContactFirstNameException>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_Calls_Repository_On_Success()
+    {
+        var command = new CreateContact(Guid.NewGuid(), "Furkan", "Bozkurt");
+
+        _factory.Create(command.Id, command.FirstName, command.LastName).Returns(default(Contact));
+
+        var exception = await Record.ExceptionAsync(() => Act(command));
+
+        exception.ShouldBeNull();
+        await _repository.Received(1).AddAsync(Arg.Any<Contact>());
+    }
+
+    #region ARRANGE
+
+    private readonly ICommandHandler<CreateContact> _commandHandler;
+    private readonly IContactRepository _repository;
+    private readonly IContactFactory _factory;
+
+    public CreateContactHandlerTests()
+    {
+        _repository = Substitute.For<IContactRepository>();
+        _factory = Substitute.For<IContactFactory>();
+
+        _commandHandler = new CreateContactHandler(_repository, _factory);
+    }
+
+    #endregion
+}
---
+++ src/Tests/Contacts/Contacts.UnitTests/Domain/ContactTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Services.Contacts.Domain.Aggregates.Contact;
+using Services.Contacts.Domain.Aggregates.Contact.Events;
+using Services.Contacts.Domain.Aggregates.Contact.ValueObjects;
+using Services.Contacts.Domain.Enums;
+using Services.Contacts.Domain.Exceptions;
+using Services.Contacts.Domain.Factories;
+using Shouldly;
+using Xunit;
+
+namespace Contacts.UnitTests.Domain;
+
+public class ContactTests
+{
+    [Fact]
+    public void AddField_Throws_ContactFieldAlreadyExistsException_When_There_Is_Already_A_Field_With_The_Same_Value_And_Type()
+    {
+        //ARRANGE
+        var packingList = GetContact();
+        packingList.AddField(new ContactField("STTTek", ContactFieldType.Company));
+            
+        //ACT
+        var exception = Record.Exception(() => packingList.AddField(new ContactField("STTTek", ContactFieldType.Company)));
+
+        //ASSERT
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<ContactFieldAlreadyExistsException>();
+    }
+
+    [Fact]
+    public void AddField_Adds_ContactFieldAdded_Domain_Event_When_Succeeded()
+    {
+        //ARRANGE
+        var contact = GetContact();
+            
+        //ACT
+        var exception = Record.Exception(() => contact.AddField(new ContactField("istanbul", ContactFieldType.Location)));
+            
+        //ASSERT
+        exception.ShouldBeNull();
+        contact.Events.Count().ShouldBe(1);
+
+        var @event = contact.Events.FirstOrDefault() as ContactFieldAdded;
+
+        @event.ShouldNotBeNull();
+        @event.Fields.All(f=>f.Value == "istanbul".ToUpperInvariant()).ShouldBe(true);
+    }
+
+
+    #region ARRANGE
+        
+    private Contact GetContact()
+    {
+        var packingList = _factory.Create(Guid.NewGuid(), "Furkan", "Bozkurt");
+        packingList.ClearEvents();
+        return packingList;
+    }
+
+    private readonly IContactFactory _factory;
+
+    public ContactTests()
+    {
+        _factory = new ContactFactory();
+    }
+
+    #endregion
+}
